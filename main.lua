6d206f649c66fe89c71673d71df197c25ea7e711
Dustin Siemers <dustin.siemers@gmail.com>
2018-02-18 09:20:36 -0600

    set up love.load, love.resize,love.keypress,love.update,love.draw

--- main.lua
+++ main.lua
@@ -15,17 +15,75 @@
 
 require 'src/Dependencies'
 
+-- physical screen dimensions
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 600
+
+-- virtual resolution dimensions
+VIRTUAL_WIDTH = 512
+VIRTUAL_HEIGHT = 288
+
 function love.load()
-    -- TODO: sound, graphics, and font tables
-    -- TODO: set up StateMachine
+    -- initialize our nearest-neighbor filter
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    -- seed the RNG
+    math.randomseed(os.time())
+
+    -- app window title
+    love.window.setTitle('Space Invaders')
+
+    -- initialize the included font
+    smallFont = love.graphics.newFont('fonts/font.ttf', 8)
+    love.graphics.setFont(smallFont)
+
+    -- initialize our table of sounds
+    sounds = {
+        ['alien_shot'] = love.audio.newSource('sounds/alien_shot.wav', 'static'),
+        ['death'] = love.audio.newSource('sounds/death.wav', 'static'),
+        ['explosion'] = love.audio.newSource('sounds/explosion.wav', 'static'),
+        ['fire'] = love.audio.newSource('sounds/fire.wav', 'static')
+    }
+
+    -- load up the graphics we'll be using throughout our states
+    gTextures = {
+        ['main'] = love.graphics.newImage('graphics/sprites.png')
+    }
+
+    -- Quads we will generate for all of our textures; Quads allow us
+    -- to show only part of a texture and not the entire thing
+    gFrames = {
+        ['aliens'] = GenerateQuads(gTextures['main'], 16, 16)
+    }
+
+    -- initialize our virtual resolution
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+        vsync = true,
+        fullscreen = false,
+        resizable = true
+    })
+
+
+    -- initialize state machine with all state-returning functions
+    gStateMachine = StateMachine {
+        ['title'] = function() return TitleScreenState() end,
+        ['play'] = function() return PlayState() end,
+        ['score'] = function() return ScoreState() end
+    }
+    gStateMachine:change('title')
+
+    -- initialize input table
+    love.keyboard.keysPressed = {}
+
 end
 
 function love.resize(w, h)
-    -- TODO
+    push:resize(w, h)
 end
 
 function love.keypressed(key)
-    -- TODO
+    -- add to our table of keys pressed this frame
+    love.keyboard.keysPressed[key] = true
 
     if key == 'escape' then
         love.event.quit()
@@ -33,11 +91,17 @@ function love.keypressed(key)
 end
 
 function love.update(dt)
-    -- TODO
+    --gStateMachine:update(dt)
+
+    love.keyboard.keysPressed = {}
 end
 
 function love.draw()
-    -- TODO
+    push:start()
+
+    gStateMachine:render()
+
+    push:finish()
 end
 
 --[[
